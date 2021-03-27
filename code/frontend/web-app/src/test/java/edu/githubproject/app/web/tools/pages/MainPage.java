42991692dfa249ece9db6117f7539d143eff4e65
Sergey2708 <sj.alee@yahoo.com>
2021-03-26 20:13:54 -0400

    initial commit

--- code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/MainPage.java
+++ code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/MainPage.java
@@ -66,7 +66,8 @@ public class MainPage extends BasePage {
     @FindBy() // TODO Andrew A
     public WebElement bikeScootTab;
 
-    @FindBy() // TODO Jane R
+    // TODO Jane R
+    @FindBy(xpath = "//span[@class='bm l0 ds bo bu c4 bv ef l1 l2 l3' and text()='Ride']")
     public WebElement getRideButton;
 
     public MainPage() {
