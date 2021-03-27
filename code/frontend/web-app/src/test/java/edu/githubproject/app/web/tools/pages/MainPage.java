45202305b6e432d79d5bb1365cfc3ac379f4bfcb
Eupatrid Theos <truvord@gmail.com>
2021-03-26 20:31:13 -0400

    Add public WebElement languageButton;

--- code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/MainPage.java
+++ code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/MainPage.java
@@ -55,8 +55,8 @@ public class MainPage extends BasePage {
 
     // Transit
 
-    @FindBy() // TODO Andrei S
-    public WebElement transitTab;
+    @FindBy(xpath = "//button[@class='dy dz bo ds e0 e1 e2 ct e3 e4 e5 e6 be e7 ca e8 e9 ea eb bi ec ed ee b8 b6 bj b7 bu c4 bv ef eq er es et ek el eu ev de w9 dm']")
+    public WebElement language;
 
     @FindBy() // TODO Vladimir H
     public WebElement transitLearnMoreButton;
