d783314250691da1de666a623370c88521441e6f
Petro Zozuliak <petro.sarpinos@gmail.com>
2021-03-26 20:17:06 -0500

    commit from b1

--- code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/MainPage.java
+++ code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/MainPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class MainPage extends BasePage {
 
-    // Eat
+    // Buy World
 
     @FindBy(xpath = "//img[@alt = 'Restaurant location']") // TODO Aigerim B
     public WebElement eatTab;
