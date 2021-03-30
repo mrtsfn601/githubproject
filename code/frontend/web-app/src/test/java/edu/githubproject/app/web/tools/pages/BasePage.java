262c88c1f94574cfc4f56737023ba3a2a77fb8d1
katez <katealexm@gmail.com>
2021-03-30 09:31:43 -0500

    resolve conflicts

--- code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/BasePage.java
+++ code/frontend/web-app/src/test/java/edu/githubproject/app/web/tools/pages/BasePage.java
@@ -22,7 +22,7 @@ public class BasePage {
     public WebElement riderLoginButton;
 
     // Signup
-//conflict 2
+    //all conflicts resolved
     @FindBy(xpath = "//button[contains(text(),'Sign up')]") // TODO Kate Z
     public WebElement signupButton;
 
